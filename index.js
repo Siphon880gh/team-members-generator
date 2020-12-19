e59e1e1b0942e4299d2baa4e09388239ab89987a
Weng Fei Fung <weffung@ucdavis.edu>
2020-12-19 08:14:15 -0800

    Setup callback for Employee inquirer questions

--- index.js
+++ index.js
@@ -1,50 +1,90 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-const teamMembers = [];
+// Globally contain all the team members so later can render to HTML
+const container = {
+    teamMembers: []
+};
 
+// Display menu
 console.log(`
 Team Members Generator
 ==========================
 By Weng Fei Fung. Generate a web-page of your development team's members.
 `);
-
-askForRole();
+askMainMenu();
 
 // Ask for role at the beginning and each time a role is chosen and its questions answered, until user chooses Finished.
-function askForRole() {
+function askMainMenu() {
+
     inquirer
         .prompt([{
             name: "role",
-            message: "Add a team member:",
+            message: "Add a team member:\n",
             type: "list",
             choices: [
                 "Manager",
                 "Engineer",
                 "Intern",
-                "Finished ->"
+                new inquirer.Separator(),
+                "Build Team Webpage ->"
             ]
         }])
         .then(answers => {
             switch (answers.role) {
                 case "Manager":
+
+                    // Ask questions
+                    const managerInfo = new Manager();
+                    managerInfo.askQuestions();
+
+                    // Add team member
+                    var newTeamMember = managerInfo.getTeamMemberObject();
+                    container.teamMembers.push(newTeamMember);
+
+                    // askMainMenu(); // Recursive
+
                     break;
                 case "Engineer":
+
+                    // Ask questions
+                    const engineerInfo = new Engineer();
+                    engineerInfo.askQuestions();
+
+                    // Add team member
+                    var newTeamMember = engineerInfo.getTeamMemberObject();
+                    container.teamMembers.push(newTeamMember);
+
+                    // askMainMenu(); // Recursive
+
                     break;
                 case "Intern":
+
+                    // Ask questions
+                    const internInfo = new Intern();
+                    internInfo.askQuestions();
+
+                    // Add team member
+                    var newTeamMember = internInfo.getTeamMemberObject();
+                    container.teamMembers.push(newTeamMember);
+
+                    // askMainMenu(); // Recursive
+
                     break;
-                case "Finished ->":
-                    writeToHTMLFile();
+                default:
+                    buildTeamPage();
                     return; // Ends Recursive
             }
-
-            askForRole(); // Recursive
         })
         .catch(error => {
             console.error("Error: " + error);
         });
-} // askForRole
+} // askMainMenu
 
-function writeToHTMLFile() {
+function buildTeamPage() {
     console.log("Outputted to load/data.json\nOpen load/ in web browser to see your team members.");
+    console.log(container);
 }
